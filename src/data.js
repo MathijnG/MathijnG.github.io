1b7d31d47badd872aa45fb6e1a224d301e4497db
Mathijn <mathijngoossens@gmail.com>
2023-01-23 17:03:40 +0100

    Added images + text

--- src/data.js
+++ src/data.js
@@ -1,10 +1,19 @@
-import {Image} from "@mantine/core";
-import Ecc1 from "./assets/ecc1.png";
+import MediaanLogo from "./assets/mediaan_logo.jpeg";
+import ApacheSpark from "./assets/Apache_Spark_logo.png";
+import Cyber from "./assets/cyber.jpg";
+import BasWorld from "./assets/basworld.jpg";
+import Unlock from "./assets/unlock.png";
+import CoolEvents from "./assets/CoolEventsLogo.png";
+import OneBlinq from "./assets/oneblinq.png";
+import NieuwLerenParkstad from "./assets/logo-NLP.png";
+import GrowBotic from "./assets/growbotic.png";
+import Webstudio7 from "./assets/logo-webstudio7.png";
+import Default from "./assets/default.jpg";
 
 const data = [
     {
         title: 'Project Management Tool',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        image: Default,
         date: new Date(),
         component:
             <>
@@ -13,75 +22,86 @@ const data = [
     },
     {
         title: 'Electric Car Charging - Mediaan',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        image: MediaanLogo,
         date: new Date("January 1, 2023"),
         component:
             <>
-                <p>During my internship at Mediaan I worked on a project with electric car chargers. The project was to create a dashboard to manage these chargers.</p>
-                <Image style={{border: "2px solid black"}} src={Ecc1} />
+                <p>During my internship at Mediaan I worked on a project with electric car chargers. The idea was to create a dashboard to manage the chargers that are located by the Mediaan office in Heerlen.</p>
             </>
     },
     {
         title: 'OneBlinq - Stuurmen',
-        image:
-            'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-        date: new Date("January 1, 2021"),
+        image: OneBlinq,
+        date: new Date("January 1, 2022"),
     },
     {
         title: 'Ticket system - BAS World',
-        image:
-            'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        image: BasWorld,
         date: new Date("June 1, 2021"),
     },
     {
         title: 'Cool Events',
-        image:
-            'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-        date: new Date("February 1, 2021"),
+        image: CoolEvents,
+        date: new Date("February 1, 2022"),
     },
     {
         title: 'Webstudio 7 projects',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        image: Webstudio7,
         date: new Date("July 1, 2020"),
     },
     {
         title: 'Pentesting medical company',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        image: Cyber,
         date: new Date("July 1, 2022"),
     },
     {
-        title: 'Unlocked - Serious Game',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        title: 'Unlock - Serious Game',
+        image: Unlock,
         date: new Date("October 1, 2020"),
+        component:
+            <>
+                <p>
+                    During my first school year I build a serious game for primary school kids. The idea of the game is to run trough a maze and escape.
+                    But to reach the end of the maze the player must solve math problems (For example: 2*7, 4*6, 8*7). You get a math problem every time you want to go trough a door.
+                    You also need to find keys and not get caught by the monster...
+                </p>
+                <p>Play the game online here: <a rel="noreferrer" href="https://simmer.io/@MathijnG/unlock" target="_blank">https://simmer.io/@MathijnG/unlock</a></p>
+            </>
     },
     {
         title: 'Growbotic',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        image: GrowBotic,
         date: new Date("January 1, 2021"),
     },
     {
         title: 'TowerDefence - Game & Leaderboard',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        image: Default,
         date: new Date("June 1, 2021"),
     },
     {
         title: 'Distributed Computing Project',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        image: ApacheSpark,
         date: new Date("June 1, 2022"),
+        component:
+            <>
+                <p>During my 4th semester I was sorted into a project group and our task was the following:</p>
+                <p>"To speed up processing intensive software we can distribute the work over multiple systems. Our aim for this project is to build an environment that will allow us to submit code to a computing cluster in a way that it is spread out and executed on each individual node in the cluster. The results of the processing can then be collected and reflected to the user. This way the code can be spread out over several servers / nodes in our cluster making the computing process much faster than running the process on a single host."</p>
+                <p>Eventually we created a application that allowed us to submit code that would run on multiple nodes with a faster runtime. We used Apache Spark with Hadoop as our cluster framework.</p>
+            </>
     },
     {
         title: '3D Terrain Generation in Unity',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        image: Default,
         date: new Date("April 1, 2021"),
     },
     {
         title: '3D Survival Game (Work in progress)',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        image: Default,
         date: new Date("October 1, 2021"),
     },
     {
         title: 'Nieuw Leren Parkstad',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+        image: NieuwLerenParkstad,
         date: new Date("July 1, 2019"),
     },
 ];
